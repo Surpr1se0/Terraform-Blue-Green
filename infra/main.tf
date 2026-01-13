3f89c2544665d41dd3223635c771d4e3506dcb52
Surpr1se0 <franciscoggouveia@gmail.com>
2026-01-13 13:52:34 +0000

    Change project structure, add new script for EC2 instance

--- infra/main.tf
+++ infra/main.tf
@@ -116,33 +116,33 @@ resource "aws_instance" "app-blue"{
   subnet_id = data.aws_subnets.default.ids[0]
   security_groups = [aws_security_group.ec2.id]
 
-  #user_data = file("user_data.sh")
-  user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Images!</h1></br>
-             <h3>(Instance BLUE)</h3>
-             </html>' | sudo tee /var/www/html/index.html
-             echo 'server {
-                       listen 80 default_server;
-                       listen [::]:80 default_server;
-                       root /var/www/html;
-                       index index.html index.htm index.nginx-debian.html;
-                       server_name _;
-                       location /images/ {
-                           alias /var/www/html/;
-                           index index.html;
-                       }
-                       location / {
-                           try_files $uri $uri/ =404;
-                       }
-                   }' | sudo tee /etc/nginx/sites-available/default
-             sudo systemctl reload nginx
-             EOF
+  user_data = file("setup-docker.sh")
+  # user_data = <<-EOF
+  #            #!/bin/bash
+  #            sudo apt-get update
+  #            sudo apt-get install -y nginx
+  #            sudo systemctl start nginx
+  #            sudo systemctl enable nginx
+  #            echo '<!doctype html>
+  #            <html lang="en"><h1>Images!</h1></br>
+  #            <h3>(Instance BLUE)</h3>
+  #            </html>' | sudo tee /var/www/html/index.html
+  #            echo 'server {
+  #                      listen 80 default_server;
+  #                      listen [::]:80 default_server;
+  #                      root /var/www/html;
+  #                      index index.html index.htm index.nginx-debian.html;
+  #                      server_name _;
+  #                      location /images/ {
+  #                          alias /var/www/html/;
+  #                          index index.html;
+  #                      }
+  #                      location / {
+  #                          try_files $uri $uri/ =404;
+  #                      }
+  #                  }' | sudo tee /etc/nginx/sites-available/default
+  #            sudo systemctl reload nginx
+  #            EOF
 
   tags = {
     Name = "app-blue"
@@ -155,33 +155,33 @@ resource "aws_instance" "app-green"{
   subnet_id       = data.aws_subnets.default.ids[0]
   security_groups = [aws_security_group.ec2.id]
 
-  #user_data = file("user_data.sh")
-  user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Images!</h1></br>
-             <h3>(Instance Green)</h3>
-             </html>' | sudo tee /var/www/html/index.html
-             echo 'server {
-                       listen 80 default_server;
-                       listen [::]:80 default_server;
-                       root /var/www/html;
-                       index index.html index.htm index.nginx-debian.html;
-                       server_name _;
-                       location /images/ {
-                           alias /var/www/html/;
-                           index index.html;
-                       }
-                       location / {
-                           try_files $uri $uri/ =404;
-                       }
-                   }' | sudo tee /etc/nginx/sites-available/default
-             sudo systemctl reload nginx
-             EOF
+  user_data = file("setup-docker.sh")
+  # user_data = <<-EOF
+  #            #!/bin/bash
+  #            sudo apt-get update
+  #            sudo apt-get install -y nginx
+  #            sudo systemctl start nginx
+  #            sudo systemctl enable nginx
+  #            echo '<!doctype html>
+  #            <html lang="en"><h1>Images!</h1></br>
+  #            <h3>(Instance Green)</h3>
+  #            </html>' | sudo tee /var/www/html/index.html
+  #            echo 'server {
+  #                      listen 80 default_server;
+  #                      listen [::]:80 default_server;
+  #                      root /var/www/html;
+  #                      index index.html index.htm index.nginx-debian.html;
+  #                      server_name _;
+  #                      location /images/ {
+  #                          alias /var/www/html/;
+  #                          index index.html;
+  #                      }
+  #                      location / {
+  #                          try_files $uri $uri/ =404;
+  #                      }
+  #                  }' | sudo tee /etc/nginx/sites-available/default
+  #            sudo systemctl reload nginx
+  #            EOF
 
   tags = {
     Name = "app-green"
